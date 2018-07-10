0f16fa4ff53e21a9a6b3185483ad75d8f09c3a5c
myGitAccount8 <eltabir819@gmail.com>
2018-07-10 11:57:58 -0400

    initial commit

---
+++ src/main/Goodafternoon.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Goodafternoon {
+
+	public static void main(String[] args) {
+
+		System.out.println("Goodafternoon everone!!!");
+
+	}
+
+}
---
+++ src/main/Goodmorning.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Goodmorning {
+
+	public static void main(String[] args) {
+
+		System.out.println("Goodmorning everyone!!!");
+
+	}
+
+}
